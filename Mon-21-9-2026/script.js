64d2b2e81641e8dd68b74a06ad17ea786ec2bb97
raghadalzghoul <alzghoulraghad15@gmail.com>
2026-09-21 15:05:07 +0300

    fist

---
+++ Mon-21-9-2026/script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+//q1
+console.log(typeof(100)) //number
+
+//q2
+console.log(typeof(73.9) )       //number
+//q3
+console.log(typeof(NaN));  //number
+//q4
+console.log(typeof("Water")) //string
+//q5
+console.log(typeof(false))//booleaan
+//q6
+console.log( typeof(9 != 11)) //booleaan
+//q7
+console.log("Orang" + "e") //orange
+//q8
+console.log("Orange" - "s") // NAN
+//q9
+console.log("4" + "8")   //48
+//q10
+console.log("name" + 3) //name3
+//q11
+console.log("name" - 3);//NAN
+//q12
+console.log(82 * "word"); //NAN
+//q13
+console.log(1 + "hello") //1hello
+//q14
+console.log( 1 + true) //2
+//q15
+console.log("hello" + true) //hellotrue
+//q16
+console.log(typeof (Infinity)) //number
+console.log(1 == '1') //true
+console.log(1 === '1')//false
+
+
